a36a2644b0a3baa86b5c2044226b3d6e69856269
Tarmstrong95 <triston95strong@gmail.com>
2019-05-31 13:19:21 -0400

    Add CRUD ops to actions

--- Routes/actionRoute.js
+++ Routes/actionRoute.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const db = require('../data/helpers/actionModel')
 const router = express.Router();
+router.use(express.json())
 
 router.get('/', (req, res) => {
     db.get()
@@ -11,6 +12,44 @@ router.get('/', (req, res) => {
         res.send(err)
     })
 })
+
+router.get('/:id', (req, res) => {
+    db.get(req.params.id)
+    .then(act => {
+        res.send(act)
+    })
+    .catch(() => {
+        res.send({message: "error"})
+    })
+})
+
+router.post('/', (req, res) => {
+    db.insert(req.body)
+    .then(act => {
+        res.send(act)
+    })
+    .catch(() => {
+        res.send({message: "error"})
+    })
+})
+
+router.put('/:id', (req, res) => {
+    db.update(req.params.id)
+    .then(act => {
+        res.send(act)
+    })
+    .catch(() => {
+        res.send({message: "error"})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    db.remove(req.params.id)
+    .then(del => {
+        res.send({message: `Deleted ${del} actions`})
+    })
+})
+
 
 
 
